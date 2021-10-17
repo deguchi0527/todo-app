d2a73b4dd9bd6de9a88bad0885f4557133037c94
deguchi0527 <ryodegu0527@gmail.com>
2021-10-17 16:40:05 +0900

    create task controller

---
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -0,0 +1,62 @@
+package com.example.todoapp.controller;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import com.example.todoapp.entity.Task;
+import com.example.todoapp.service.TaskService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TaskController {
+
+  @Autowired
+  TaskService taskService;
+
+  // タスク表示画面
+  @GetMapping
+  public ModelAndView top() {
+    ModelAndView mav = new ModelAndView();
+    List<Task> tasks = taskService.findAllTask();
+    mav.setViewName("/top");
+    mav.addObject("tasks", tasks);
+
+    return mav;
+  }
+
+  // タスクの追加画面
+  @GetMapping("/new")
+  public ModelAndView newContent() {
+    ModelAndView mav = new ModelAndView();
+    // form用の空のentityを準備
+    Task task = new Task();
+    mav.setViewName("/new");
+    mav.addObject("formModel", task);
+
+    return mav;
+  }
+
+  // タスクの追加処理
+  @PostMapping("/add")
+  public ModelAndView addContent(@ModelAttribute("formModel") Task task, @RequestParam String limit) {
+    // パラメータから送られてくる値をTimestamp型に変換するための処理
+    String limitDateFormat = limit + ":00";
+    String limitDateReplace = limitDateFormat.replace("T", " ");
+    Timestamp limitDate = Timestamp.valueOf(limitDateReplace);
+
+    // entityにlimit_dateをセット
+    task.setLimit_date(limitDate);
+
+    // タスクをテーブルに格納
+    taskService.saveTask(task);
+
+    return new ModelAndView("redirect:/");
+  }
+}
